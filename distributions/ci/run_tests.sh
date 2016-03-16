eada5a960b78b243140c2d9da84662fad813d6ee
mbillingr <mbillingr@users.noreply.github.com>
2016-03-16 09:46:09 +0100

    Create run_tests.sh

---
+++ distributions/ci/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variables defined
+# in the .travis.yml in the top level folder of the project.
+
+echo "============================"
+echo "============================"
+echo "Testing Environment:"
+python --version
+echo "============================"
+echo "============================"
+
+if [[ "$COVERAGE" == "true" ]]; then
+    nosetests -v --with-coverage scot
+else
+    nosetests -v
+fi
