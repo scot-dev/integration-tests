5023c639850ee3e5238be968b9dfbfab1f39b1a4
mbillingr <mbillingr@users.noreply.github.com>
2016-03-16 10:00:56 +0100

    Update test_hello.py

--- sources/tests/test_hello.py
+++ sources/tests/test_hello.py
@@ -5,9 +5,9 @@ from sources.covered import hello
 
 class TestHello(unittest.TestCase):
     def test_once(self):
-        self.assert_equal(hello(1), "Hello World!")
+        self.assertEqual(hello(1), "Hello World!")
     def test_multi(self):
         hi = hello(10)
-        self.assert_equal(len(hi) == 10)
+        self.assertEqual(len(hi) == 10)
         for h in hi:
-            self.assert_equal(h, "Hello World!")
+            self.assertEqual(h, "Hello World!")
