624dfebc9f23d4987cde267e7c0a5c7bbc45f215
mbillingr <mbillingr@users.noreply.github.com>
2016-03-29 09:30:19 +0200

    Update covered.py

--- sources/covered.py
+++ sources/covered.py
@@ -4,5 +4,6 @@ def hello(count):
     s = "Hello World!"
     if count == 1:
         return s
-    result = tuple([s] * count)
+    result = [s] * count
+    result = tuple(result)
     return result
